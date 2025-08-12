81e6acb4f3835b91bec1ced50a4aeee4f65daad4
Shashank Shukla <shashankjti22@gmail.com>
2025-08-12 23:55:44 +0530

    Create Home.js

---
+++ Week6/React HOL 2/studentapp/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+        <div className="Home">  
+            <h3>Welcome to the Home Page of Student Management Portal.</h3>  
+        </div>
+    )
+  }
+}
