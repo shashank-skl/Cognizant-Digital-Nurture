945bbc11f32794cf68113dfd455805600a36dec2
Shashank Shukla <shashankjti22@gmail.com>
2025-08-12 23:54:52 +0530

    Create About.js

---
+++ Week6/React HOL 2/studentapp/src/Components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="About">
+        <h3>Welcome to the About the Student Management Portal</h3>
+      </div>
+    )
+  }
+}
